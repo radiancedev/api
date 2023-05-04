6cdc22e8dc206cc10fd47ed5f6ed5ceef9684ad2
Irisu <nekomarisakirisame@gmail.com>
2023-05-04 10:36:38 -0400

    add ability to serverless vercel

---
+++ vercel.ts
@@ -0,0 +1,14 @@
+import { Application, Context } from "@bismuthmoe/arcadia";
+import { Routes } from "./routes/main";
+const app = new Application();
+
+app.on("error", (ctx: Context, err) => {
+    console.log(err);
+    ctx.status(500);
+    ctx.json({ code: 500, error: "Internal server error" });
+})
+
+app.express.set("trust proxy", true);
+app.register(Routes);
+
+export default app.express;
